b1e0b2aba132e5e47ae1eddc5c9d0f29f13cac84
balazs.saros <balazs.saros@gmail.com>
2018-06-07 16:52:09 +0200

    outline of ideas: inefficient continous search with thumbnails and names

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+const API_KEY = '937517d8496fc712bbb2e6291ca03b27';
+
+function charactersEndpoint(startsWith) {
+    return `https://gateway.marvel.com/v1/public/characters?nameStartsWith=${startsWith}&apikey=${API_KEY}&limit=100`
+}
+
+class Card extends Component {
+    render() {
+        return (
+            <div>
+                <h3>{(this.props.data || {}).name}</h3>
+                <img src={((this.props.data || {}).thumbnail || {}).path + '.jpg'} width="120"/>
+            <p>{(this.props.data || {}).description}</p>
+                <hr/>
+            </div>
+        );
+    }
+}
+
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            searchQuery: '',
+            results: []
+        };
+    }
 
-    state = {
-        contacts: []
-    };
+    handleSearchInput(event) {
+        const typedQuery = event.target.value;
+        this.setState({searchQuery: typedQuery});
 
-    componentDidMount() {
-        fetch('https://gateway.marvel.com/v1/public/characters?nameStartsWith=th&apikey=937517d8496fc712bbb2e6291ca03b27')
-            .then(response => response.json()).then(result => {
-                console.log(result);
+        if(typedQuery !== '') {
+            this.getSearchResults(typedQuery);
+        } else {
+            this.setState({results: []});
+        }
+    }
+
+    getSearchResults(startsWith) {
+        fetch(charactersEndpoint(startsWith))
+            .then(response => response.json())
+            .then(result => {
+                this.setState({results: result.data.results});
+                console.log(result.data.results);
         });
+    }
 
+    createResultList() {
+        return this.state.results.map((result, index) => <Card data={result} key={index}/>);
     }
 
     render() {
         return (
-            <h1>hello world</h1>
+            <div>
+            <input type="text" value={this.state.searchQuery} onChange={this.handleSearchInput.bind(this)}/>
+            {this.createResultList()}
+            </div>
         );
     }
 }
